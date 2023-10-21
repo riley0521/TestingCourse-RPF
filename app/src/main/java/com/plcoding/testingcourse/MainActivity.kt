c56c6801d036b26595aa0076c93097baacb63685
Riley Farro <rileyfarro@gmail.com>
2023-10-21 20:19:17 +0800

    - Testing Intents. Yey

--- app/src/main/java/com/plcoding/testingcourse/MainActivity.kt
+++ app/src/main/java/com/plcoding/testingcourse/MainActivity.kt
@@ -1,25 +1,19 @@
 package com.plcoding.testingcourse
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.plcoding.testingcourse.part11.presentation.ProfileScreen
-import com.plcoding.testingcourse.part11.presentation.previewProfileState
-import com.plcoding.testingcourse.part7.presentation.ProfileViewModel
+import com.plcoding.testingcourse.part12.presentation.ProfileActivity
 import com.plcoding.testingcourse.ui.theme.TestingCourseTheme
 
 class MainActivity : ComponentActivity() {
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -30,7 +24,18 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    ProfileScreen(state = previewProfileState())
+//                    ProfileScreen(state = previewProfileState())
+
+                    Button(onClick = {
+                        Intent(applicationContext, ProfileActivity::class.java).also {
+                            it.putExtra("TEST_EXTRA", "top secret")
+                            it.action = "MY_ACTION"
+
+                            startActivity(it)
+                        }
+                    }) {
+                        Text(text = "Send intent")
+                    }
                 }
             }
         }
